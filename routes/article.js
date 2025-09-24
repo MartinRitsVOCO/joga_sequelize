1949a88eb61c43addefc28dafd9d7c69b296f328
MartinRitsVOCO <martin.ritsberg@voco.ee>
2025-09-24 12:58:29 +0300

    Delete article data

--- routes/article.js
+++ routes/article.js
@@ -6,7 +6,6 @@ import adminArticleController from '../controllers/admin/article.js';
 
 router.get('/', articleController.getAll);
 router.get('/slug/:slug', articleController.getBySlug);
-
 router.post('/admin/add', adminArticleController.create);
 router.delete('/admin/delete/:id', adminArticleController.delete);
 router.put('/admin/update/:id', adminArticleController.update);
